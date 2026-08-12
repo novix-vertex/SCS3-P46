b72b30f17a5c3d3be5607a08b15d5010ed45ff72
chirag jain <jchirag0610@gmail.com>
2026-08-12 19:28:20 +0530

    certifications done

--- js/certificates/certificatesData.js
+++ js/certificates/certificatesData.js
@@ -1,30 +1,30 @@
 export const certificatesData = [
     {
         number: "01",
-        title: "Project Management Certification",
-        provider: "Certification Provider",
-        category: "PM",
-        year: "2026"
+        title: "Certified Scrum Master (CSM)",
+        provider: "Scrum Alliance",
+        category: "CSM",
+        year: "2024"
     },
     {
         number: "02",
-        title: "Project Management Certification",
-        provider: "Certification Provider",
-        category: "PM",
+        title: "AI Engineering Master Program (IBM)",
+        provider: "Simplilearn",
+        category: "AI",
         year: "2026"
     },
     {
         number: "03",
-        title: "Project Management Certification",
-        provider: "Certification Provider",
-        category: "PM",
+        title: "Data Analytics Program",
+        provider: "Ira Skills",
+        category: "Data Analytics",
         year: "2026"
     },
     {
         number: "04",
-        title: "Project Management Certification",
-        provider: "Certification Provider",
-        category: "PM",
-        year: "2026"
+        title: "Certified Salesforce Administrator",
+        provider: "Salesforce",
+        category: "ADMIN",
+        year: "2024"
     }
 ] 
